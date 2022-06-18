91195a70aee4fc3e6f964f95a892123b5cb69c73
baonguyen25 <nguyenminhgiabao2501@gmail.com>
2022-06-18 15:22:23 +0700

    implement get user's detail and delete a user methods

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,14 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = []
+let users = []
 
+// Get a list of all users
 router.get('/', (req, res) => {
     res.send(users);
 });
 
+// Create a new user
 router.post('/', (req, res) => {
     const user = req.body; 
     const userWithID = {...user, id: uuidv4()}; // create new user object that have ID
@@ -16,4 +18,20 @@ router.post('/', (req, res) => {
     users.push(userWithID);
     res.send(`User with the name ${user.firstName} added to the database!`);
 });
+
+// Get user's information by id
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
+});
+
+// Delete a user
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with the id ${id} deleted from the database!`);
+});
+
 export default router;
